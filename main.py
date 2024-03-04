a14d7f347db1e7b179901e74ee4ba8bf95783ee4
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2024-03-04 11:47:15 -0600

    added class to move files to specified folder

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ from watchdog.events import LoggingEventHandler
 # duplicate backslashes so program doesn't run into Unicode error
 sourceDir = "C:\\Users\\bndy1\\Downloads"
 
-with os.scandir(sourceDir) as entries:
-    for entry in entries:
-        print(entry.name)
-
+class MoveFileHandler(FileSystemEventHandler):
+    def modified(self, event):
+        with os.scandir(sourceDir) as entries:
+            for entry in entries:
+                name = entry.name
+                print(entry.name)
 
 # monitors the current directory recursively for file system changes and logs them to the console
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
+    path = sourceDir
+    event_handler = MoveFileHandlerr()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
