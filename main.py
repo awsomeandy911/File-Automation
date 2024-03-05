e5d8d9fe6af4675f45d6a2554b530f63b646b65c
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2024-03-04 20:06:33 -0600

    updated MoveFileHandler class and added move function

--- main.py
+++ main.py
@@ -12,12 +12,26 @@ musicDir = "C:\\Users\\bndy1\\Music"
 imageDir = "C:\\Users\\bndy1\\Pictures"
 videoDir = "C:\\Users\\bndy1\\Videos"
 
+def move(destination, event, name):
+    # needs work
+    fileExists = os.scandir
+
+# class that handles the moving of files
 class MoveFileHandler(FileSystemEventHandler):
     def modified(self, event):
         with os.scandir(sourceDir) as entries:
             for entry in entries:
                 name = entry.name
                 destination = sourceDir
+                if name.endswith('.mp3'):
+                    destination = musicDir
+                    # needs move function
+                elif name.endswith('.mp4') or name.endswith('.mov'):
+                    destination = videoDir
+                    # move function
+                elif name.endswith('.png') or name.endswith('.jpeg') or name.endswith('.jpg'):
+                    destination = imageDir
+                    # move function
                 print(entry.name)
 
 # monitors the current directory recursively for file system changes and logs them to the console
